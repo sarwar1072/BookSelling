cd27812be9feca9721dba0cf737ea78b356505d5
sarwar <sarwar.cse.diu@gmail.com>
2022-05-16 16:01:58 +0600

    interface implement

--- BookSell/Framework/Services/IProductService.cs
+++ BookSell/Framework/Services/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Framework.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,14 @@ namespace Framework.Services
 {
    public interface IProductService:IDisposable
     {
+        void AddProduct(Product product);
+        Product Delete(int id);
+        void EditProduct(Product product);
+        Product GetId(int id);
+        (IList<Product> product, int total, int totalDisplay) GetProduct(int pageindex, int pagesize,
+                                                                               string searchText, string orderBy);
+        IList<Category> GetCategories();
+        IList<CoverType> GetCoverTypes();
 
     }
 }
