fd22f9190fdc51cad477aede61928186269a191a
sarwar mahmud milon <sarwar.cse.diu@gmail.com>
2023-08-15 10:02:41 +0600

    added custeomer to option

--- BookSell/Membership/Seeds/IdenSD.cs
+++ BookSell/Membership/Seeds/IdenSD.cs
@@ -6,7 +6,7 @@ namespace Membership.Seeds
 {
     public static class IdenSD
     {
-        public const string Role_User_Indi = "Individual Customer";
+        public const string Role_User_Indi = "User";
         public const string Role_User_Comp = "Company Customer";
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
