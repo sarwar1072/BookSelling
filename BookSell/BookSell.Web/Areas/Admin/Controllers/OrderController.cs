d03b50a7a5a710175a637c527145f9c239e044e5
sarwar mahmud milon <sarwar.cse.diu@gmail.com>
2023-09-10 01:48:15 +0600

    order

--- BookSell/BookSell.Web/Areas/Admin/Controllers/OrderController.cs
+++ BookSell/BookSell.Web/Areas/Admin/Controllers/OrderController.cs
@@ -1,56 +1,49 @@
 ﻿using Autofac;
-using BookSell.Web.Areas.Admin.Models;
 using BookSell.Web.Areas.Admin.Models.OrderDetailsModel;
+using BookSell.Web.Areas.Admin.Models;
 using BookSell.Web.Models;
-using Framework.Services;
 using Framework.UnitOfWorkPro;
+using Microsoft.AspNetCore.Mvc;
 using Membership.Seeds;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Razorpay.Api;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookSell.Web.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    [Authorize(Roles = IdenSD.Role_Admin)]
+	[Area("Admin")]
+	[Authorize(Roles = IdenSD.Role_Admin)]
+	public class OrderController : Controller
+	{
+		ISellUnitOfWork _sellUnitOfWork;
+		public OrderDetailsVM OrderDetailsVM { get; set; }
 
-    public class OrderController : Controller
-    {
-        ISellUnitOfWork _sellUnitOfWork;
-        public OrderDetailsVM  OrderDetailsVM { get; set; }
+		public OrderController(ISellUnitOfWork sellUnitOfWork)
+		{
+			_sellUnitOfWork = sellUnitOfWork;
+		}
+		public IActionResult Index()
+		{
+			var model = Startup.AutofacContainer.Resolve<OrderDetailModel>();
 
-        public OrderController(ISellUnitOfWork sellUnitOfWork)
-        {
-              _sellUnitOfWork=sellUnitOfWork;
-    }
-        public IActionResult Index()
-        {
-            var model = Startup.AutofacContainer.Resolve<OrderDetailModel>();
+			return View(model);
+		}
+		public IActionResult Details(int id)
+		{
+			OrderDetailsVM = new OrderDetailsVM()
+			{
+				OrderHeader = _sellUnitOfWork.OrderHeaderRepository.GetFirstOrDefault(u => u.Id == id),
+				OrderDetails = _sellUnitOfWork.OrderDetailsRepository.GetAll(u => u.OrderId == id, includeProperties: "Product"),
+			};
+			//var model = new OrderDetailVM();
+			//model.Load(id);
+			return View(OrderDetailsVM);
+		}
 
-            return View(model);
-        }
-        public IActionResult Details(int id)
-        {
-            OrderDetailsVM = new OrderDetailsVM()
-            {
-                OrderHeader = _sellUnitOfWork.OrderHeaderRepository.GetFirstOrDefault(u=>u.Id==id),
-                OrderDetails=_sellUnitOfWork.OrderDetailsRepository.GetAll(u=>u.OrderId==id, includeProperties:"Product"),
-            };
-            //var model = new OrderDetailVM();
-            //model.Load(id);
-            return View(OrderDetailsVM);
-        }
-
-        public IActionResult GetOrder()
-        {
-            var tableModel = new DataTablesAjaxRequestModel(Request);
-            var model = Startup.AutofacContainer.Resolve<OrderDetailModel>();
-            var data = model.GetOrder(tableModel);
-            return Json(data);
-        }       
-    }
+		public IActionResult GetOrder()
+		{
+			var tableModel = new DataTablesAjaxRequestModel(Request);
+			var model = Startup.AutofacContainer.Resolve<OrderDetailModel>();
+			var data = model.GetOrder(tableModel);
+			return Json(data);
+		}
+	}
 }
