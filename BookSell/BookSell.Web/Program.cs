468b6f6e00aedc00497a440a73cc657fbe0d96ca
sarwar mahmud <sarwar.cse.diu@gmail.com>
2023-09-29 09:50:39 +0600

    docker file and doc

--- BookSell/BookSell.Web/Program.cs
+++ BookSell/BookSell.Web/Program.cs
@@ -24,6 +24,7 @@ namespace BookSell.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80");
                 });
     }
 }
